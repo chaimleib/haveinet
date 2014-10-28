42094a944ec5859bf09fb2b5fd643cb1c45af334
Chaim-Leib Halbert <chaim.leib.halbert@gmail.com>
2014-10-28 16:50:15 -0700

    ver bump; fixed err output; reduced # tries to 3

--- haveinet.sh
+++ haveinet.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
-# haveinet v0.0.1
+# haveinet v0.0.2
 
-VERSION=0.0.1
+VERSION=0.0.2
 
 # For OSX
 # try 5x, 0.1sec intervals, timeout 1sec
-PING_CMD='ping -c5 -i0.1 -t1 -no "$TEST_ADDR" 2>&1 1>/dev/null'
+PING_CMD='ping -c3 -i0.1 -t1 -no "$TEST_ADDR" 2>&1 1>/dev/null'
 
 function showUsage() {
     echo "Usage: $0 [-q | -a ADDR | -v | -h]"
     echo
     echo "Options:"
     echo "  -a      Test against this address (default: 8.8.8.8)"
-    echo "  -q      Suppress output, return status code only"
+    echo "  -q      Suppress errors, stdout only"
+    echo "  -s      Suppress errors and stdout, return status code only"
     echo "  -v      Show version info"
     echo "  -h      Show this help"
 }
@@ -26,13 +27,16 @@ function showHelp() {
     showUsage
 }
     
-while getopts ":qhva:" opt; do
+while getopts ":qshva:" opt; do
     case "$opt" in
         a)
             TEST_ADDR="$OPTARG"
             ;;
         q)
             QUIET=y
+            ;;
+        s)
+            SILENT=y
             ;;
         h)
             showHelp
@@ -53,22 +57,22 @@ done
 
 [[ -n "$TEST_ADDR" ]] || TEST_ADDR=8.8.8.8
 
-ERRORS="$(eval $PING_CMD)"
+ERRORS="$(eval $PING_CMD 2>&1)"
 RESULT="$?"
 ERRORS="$(echo "$ERRORS" | uniq)"
 
-[[ "$QUIET" == 'y' ]] && exit $RESULT
+[[ "$SILENT" == 'y' ]] && exit $RESULT
 
 case "$RESULT" in
     0)
         echo "Connected to internet"
         ;;
     2)
-        [[ -n "$ERRORS" ]] && echo "$ERRORS" >&2
+        [[ -n "$ERRORS" ]] && [[ -z "$QUIET" ]] && echo "$ERRORS" >&2
         echo "Not connected to internet"
         ;;
     *)
-        [[ -n "$ERRORS" ]] && echo "$ERRORS" >&2
+        [[ -n "$ERRORS" ]] && [[ -z "$QUIET" ]] && echo "$ERRORS" >&2
         echo "ping error: $RESULT"
         echo "ERROR: aborted"
         ;;
